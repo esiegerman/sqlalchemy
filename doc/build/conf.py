e4f452680bdc2ab6112dd521f84cb6236e792fd6
Eric Siegerman <eric.siegerman@rci.rogers.com>
2015-03-05 23:14:49 -0500

    Version 0.9.8+rogers3

--- doc/build/conf.py
+++ doc/build/conf.py
@@ -92,7 +92,7 @@ copyright = u'2007-2014, the SQLAlchemy authors and contributors'
 # The short X.Y version.
 version = "0.9"
 # The full version, including alpha/beta/rc tags.
-release = "0.9.8+rogers2"
+release = "0.9.8+rogers3"
 
 release_date = "October 13, 2014"
 
