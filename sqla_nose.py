259a54f2f77adf8011b4c79b5c926e91231bfcd5
Mike Bayer <mike_mp@zzzcomputing.com>
2015-03-06 14:42:11 -0500

    patch to allow tests to run under older nosetests

--- sqla_nose.py
+++ sqla_nose.py
@@ -6,6 +6,8 @@ This script is a front-end to "nosetests" which
 installs SQLAlchemy's testing plugin into the local environment.
 
 """
+import os
+import re
 import sys
 import imp
 import nose
@@ -20,5 +22,24 @@ for pth in ['./lib']:
 path = "lib/sqlalchemy/testing/plugin/noseplugin.py"
 noseplugin = imp.load_source("noseplugin", path)
 
+nose_ver = tuple(int(d) for d in re.findall(r'\d+', nose.__version__))
+if nose_ver <= (0, 10, 4):
+    # nose version 0.10.4 did not include the "addplugins"
+    # option, so implement directly
+    from nose.core import Config
+    from nose.core import all_config_files
+    from nose.plugins.manager import DefaultPluginManager
 
-nose.main(addplugins=[noseplugin.NoseSQLAlchemy()])
+    class AddPluginManager(DefaultPluginManager):
+        def loadPlugins(self):
+            super(AddPluginManager, self).loadPlugins()
+            self.addPlugin(noseplugin.NoseSQLAlchemy())
+
+    manager = AddPluginManager()
+    config = Config(
+        env=os.environ,
+        files=all_config_files(), plugins=manager)
+
+    nose.main(config=config)
+else:
+    nose.main(addplugins=[noseplugin.NoseSQLAlchemy()])
